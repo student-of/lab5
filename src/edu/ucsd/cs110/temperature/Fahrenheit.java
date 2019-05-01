3c9502c005f84d47fa4f6f764a8841d9f4146a51
Random_Civvy <vomalexander@gmail.com>
2019-05-01 14:17:42 -0700

    Initial Commit

---
+++ src/edu/ucsd/cs110/temperature/Fahrenheit.java
@@ -0,0 +1,24 @@
+package edu.ucsd.cs110.temperature;
+public class Fahrenheit extends Temperature
+{
+    public Fahrenheit(float t)
+    {
+        super(t);
+    }
+
+    @Override
+    public Temperature toCelsius() {
+        float c = (this.getValue() - 32) * 5 / 9;
+        return new Celsius(c);
+    }
+
+    @Override
+    public Temperature toFahrenheit() {
+        return this;
+    }
+
+    public String toString()
+    {
+        return this.getValue() + " F";
+    }
+}
